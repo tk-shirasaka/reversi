50347c7f9aaeaa8a03a0968ee0c7a59d3145b324
shirasaka <tk.shirasaka@gmail.com>
2017-05-18 01:28:34 +0900

    Refactoring

--- game/field.go
+++ game/field.go
@@ -28,7 +28,48 @@ func (f *field) itelator(i int, j int, i_offset int, j_offset int, callback func
 	}
 }
 
-func (f *field) check() {
+func (f *field) check(i int, j int, i_offset int, j_offset int) []*cell {
+	result := []*cell{}
+	cells := []*cell{}
+	step := 0
+	f.itelator(i, j, i_offset, j_offset, func(c *cell) bool {
+		ret := true
+		cells = append(cells, c)
+		switch step {
+		case 0:
+			ret = c.isnot(BLACK | WHITE)
+			step++
+			break
+		case 1:
+			ret = c.is(BLACK|WHITE) && c.isnot(f.turn)
+			step++
+			break
+		case 2:
+			ret = c.is(BLACK|WHITE) && c.isnot(f.turn)
+			if c.is(f.turn) {
+				result = cells
+				f.cells[i][j].change(PUTABLE)
+			}
+			break
+		}
+		return ret
+	})
+	return result
+}
+
+func (f *field) checkCell(i int, j int) []*cell {
+	cells := append(f.check(i, j, -1, -1), f.check(i, j, -1, 0)...)
+	cells = append(cells, f.check(i, j, -1, 1)...)
+	cells = append(cells, f.check(i, j, 0, -1)...)
+	cells = append(cells, f.check(i, j, 0, 1)...)
+	cells = append(cells, f.check(i, j, 1, -1)...)
+	cells = append(cells, f.check(i, j, 1, 0)...)
+	cells = append(cells, f.check(i, j, 1, 1)...)
+
+	return cells
+}
+
+func (f *field) checkCells() {
 	if f.dirty == false {
 		return
 	}
@@ -42,94 +83,31 @@ func (f *field) check() {
 	}
 	for i, line := range f.cells {
 		for j, _ := range line {
-			var step int
-			checker := func(c *cell) bool {
-				ret := true
-				if c == &f.cells[i][j] {
-					step = 0
-				}
-				switch step {
-				case 0:
-					ret = c.is(BLANK)
-					break
-				case 1:
-					ret = c.is(BLACK|WHITE) && c.isnot(f.turn)
-					break
-				case 2:
-					ret = c.is(BLACK|WHITE) && c.isnot(f.turn)
-					if c.is(f.turn) {
-						f.cells[i][j].change(PUTABLE)
-					}
-					break
-				}
-				if ret {
-					step++
-				}
-				return ret
-			}
-
-			f.itelator(i, j, -1, -1, checker)
-			f.itelator(i, j, -1, 0, checker)
-			f.itelator(i, j, -1, 1, checker)
-			f.itelator(i, j, 0, -1, checker)
-			f.itelator(i, j, 0, 1, checker)
-			f.itelator(i, j, 1, -1, checker)
-			f.itelator(i, j, 1, 0, checker)
-			f.itelator(i, j, 1, 1, checker)
+			f.checkCell(i, j)
 		}
 	}
 }
 
 func (f *field) Select(j int, i int) {
-	if i < 1 || i > 8 || j < 1 || j > 8 {
-		return
-	}
 	i--
 	j--
-	f.check()
+	if i < 0 && i > 7 && j < 0 && j > 7 {
+		return
+	}
+	cells := f.checkCell(i, j)
 	if f.cells[i][j].is(PUTABLE) {
-		step := 0
-		next := WHITE
-		if f.turn == next {
-			next = BLACK
+		f.cells[i][j].change(f.turn)
+		for _, cell := range cells {
+			cell.change(f.turn)
 		}
-		selector := func(c *cell) bool {
-			if step == 0 {
-				c.color = f.turn
-				step = 1
-			} else if step == 1 && c.is(next) {
-				step = 2
-			} else if step == 2 && c.is(f.turn) {
-				step = 3
-				return false
-			} else if step == 3 {
-				step = 4
-			} else if step == 4 && c.is(next) {
-				step = 5
-				c.color = f.turn
-			} else {
-				step = 0
-				return false
-			}
-			return true
+		switch f.turn {
+		case BLACK:
+			f.turn = WHITE
+			break
+		case WHITE:
+			f.turn = BLACK
+			break
 		}
-		f.itelator(i, j, -1, -1, selector)
-		f.itelator(i, j, -1, -1, selector)
-		f.itelator(i, j, -1, 0, selector)
-		f.itelator(i, j, -1, 0, selector)
-		f.itelator(i, j, -1, 1, selector)
-		f.itelator(i, j, -1, 1, selector)
-		f.itelator(i, j, 0, -1, selector)
-		f.itelator(i, j, 0, -1, selector)
-		f.itelator(i, j, 0, 1, selector)
-		f.itelator(i, j, 0, 1, selector)
-		f.itelator(i, j, 1, -1, selector)
-		f.itelator(i, j, 1, -1, selector)
-		f.itelator(i, j, 1, 0, selector)
-		f.itelator(i, j, 1, 0, selector)
-		f.itelator(i, j, 1, 1, selector)
-		f.itelator(i, j, 1, 1, selector)
-		f.turn = next
 		f.dirty = true
 	}
 }
@@ -138,7 +116,7 @@ func (f *field) String() string {
 	str := "  1 2 3 4 5 6 7 8\n"
 	black := 0
 	white := 0
-	f.check()
+	f.checkCells()
 	for i, line := range f.cells {
 		str += strconv.Itoa(i+1) + " "
 		for _, val := range line {
